ef2d8d22fbb1f99e7c3a6986a448fbf63454cad0
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-05-16 05:26:06 +0700

    membuat build project

--- src/test/kotlin/gradle/AppTest.kt
+++ src/test/kotlin/gradle/AppTest.kt
@@ -4,11 +4,13 @@
 package gradle
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class AppTest {
     @Test fun testAppHasAGreeting() {
         val classUnderTest = App()
         assertNotNull(classUnderTest.greeting, "app should have a greeting")
+        assertEquals(classUnderTest.greeting, "Hello world.")
     }
 }
